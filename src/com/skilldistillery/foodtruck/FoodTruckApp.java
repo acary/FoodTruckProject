a681994fb756c06d1c3ca42e302ccd3f2e1206a2
Andy Cary <info@andycary.com>
2022-02-26 17:50:56 -0500

    remove static methods in app besides main

--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -21,11 +21,11 @@ public class FoodTruckApp {
 				attending = false;
 				break;
 			}
-			
+
 			System.out.println("Enter a food type:");
 			foodType = sc.next();
 			foodType = foodType.trim();
-			
+
 			System.out.println("Enter a rating:");
 			rating = sc.nextDouble();
 
@@ -34,61 +34,48 @@ public class FoodTruckApp {
 
 		} while (attending);
 
-		greet(sc);
+		do {
+			for (;;) {
+				// PRINT MENU
+				System.out.println();
+				System.out.println("******************************");
+				System.out.println("Please make a selection:");
 
-		sc.close();
-	}
-
-	private static void greet(Scanner sc) {
-		System.out.println();
-		System.out.println("******************************");
-		System.out.println("Please make a selection:");
-		showMenu();
-		getSelection(sc);
-	}
-
-	private static void getSelection(Scanner sc) {
-		String selection = sc.next();
-		switch (selection) {
-		case "1":
-			presentRodeo(sc);
-			break;
-		case "2":
-			System.out.println();
-			System.out.println("Average rating: TBD");
-			greet(sc);
-			break;
-		case "3":
-			System.out.println();
-			System.out.println("Winning food truck: TBD");
-			greet(sc);
-			break;
-		case "Q":
-		case "q":
-		case "quit":
-		case "QUIT":
-			System.out.println("Thanks for visiting!");
-			System.out.println();
-			System.exit(0);
-		}
-	}
-
-	private static void showMenu() {
-		System.out.println();
-		System.out.println("1: Display all food trucks");
-		System.out.println("2: Display average rating");
-		System.out.println("3: Display winning food truck");
-		System.out.println("Q: Quit");
-		System.out.println("******************************");
-	}
-
-	public static void presentRodeo(Scanner sc) {
-		System.out.println("\nATTENDING FOOD TRUCKS:\n");
-		FoodTruck ft = new FoodTruck();
-		FoodTruck[] foodTruckRodeo = ft.getTrucks();
-		for (FoodTruck current : foodTruckRodeo) {
-			System.out.println(current.toString());
-		}
-		greet(sc);
+				System.out.println();
+				System.out.println("1: Display all food trucks");
+				System.out.println("2: Display average rating");
+				System.out.println("3: Display winning food truck");
+				System.out.println("Q: Quit");
+				System.out.println("******************************");
+				
+				// GET SELECTION
+				String selection = sc.next();
+				switch (selection) {
+				case "1":
+					System.out.println("\nATTENDING FOOD TRUCKS:\n");
+					FoodTruck ft = new FoodTruck();
+					FoodTruck[] foodTruckRodeo = ft.getTrucks();
+					for (FoodTruck current : foodTruckRodeo) {
+						System.out.println(current.toString());
+					}
+					break;
+				case "2":
+					System.out.println();
+					System.out.println("Average rating: TBD");
+					break;
+				case "3":
+					System.out.println();
+					System.out.println("Winning food truck: TBD");
+					break;
+				case "Q":
+				case "q":
+				case "quit":
+				case "QUIT":
+					System.out.println("Thanks for visiting!");
+					System.out.println();
+					System.exit(0);
+				}
+			}
+		} while (attending);
 	}
 }
