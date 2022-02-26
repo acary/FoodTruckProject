1ff65d50b354b56fa5e67a33d4d5c52017ca5fda
Andy Cary <info@andycary.com>
2022-02-26 10:00:48 -0500

    set up class and test instances

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,50 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	static int nextTruckId = 0;
+	private int uniqueId;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	public FoodTruck() {
+		this.uniqueId = ++nextTruckId;
+	}
+	
+	public String toString() {
+		String output = "";
+		output += "" + uniqueId + ": ";
+		output += name + " (" + foodType + ")";
+		output += " - Rating: " + rating;
+		System.out.println(output);
+		return output;
+	}
 
+	public int getNextTruckId() {
+		return nextTruckId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -3,8 +3,23 @@ package com.skilldistillery.foodtruck;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		FoodTruck ft1 = new FoodTruck();
+		ft1.setName("Axle Grease");
+		ft1.setFoodType("American");
+		ft1.setRating(4.5);
+		ft1.toString();
+		
+		FoodTruck ft2 = new FoodTruck();
+		ft2.setName("Bok Choy Boy");
+		ft2.setFoodType("Asian");
+		ft2.setRating(4.3);
+		ft2.toString();
+		
+		FoodTruck ft3 = new FoodTruck();
+		ft3.setName("Classic Diner");
+		ft3.setFoodType("Continental");
+		ft3.setRating(3.5);
+		ft3.toString();
 	}
-
 }
